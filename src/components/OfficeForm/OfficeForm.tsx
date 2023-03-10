3c7bbde7260cac8bacf776e0eba65127d5b4b7eb
Sebastian <sebastian@baier.codes>
2023-03-10 19:52:15 +0100

    fix strange time behavior

--- src/components/OfficeForm/OfficeForm.tsx
+++ src/components/OfficeForm/OfficeForm.tsx
@@ -201,15 +201,6 @@ export default function OfficeForm({
 				zipCode.length > 0
 			) {
 				let openHours: BusinessHours[] = [];
-				for (let index = 0; index < 5; index++) {
-					openHours.push({
-						openingTime: +(+(8 * 60 * 60 * 1000).toFixed(0)),
-						closingTime: +(+(16 * 60 * 60 * 1000).toFixed(0)),
-						lunchStart: +(+(12 * 60 * 60 * 1000).toFixed(0)),
-						lunchEnd: +(+(13 * 60 * 60 * 1000).toFixed(0)),
-					});
-				}
-				console.log(openHours);
 
 				const newDocData: Doctor = {
 					id: currdoctor ? currdoctor.id : 0, // TODO: get id from blockchain
